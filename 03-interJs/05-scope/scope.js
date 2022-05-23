9f56f944b5264039cb2d4fe1f143c82a17b6a76e
Ze <zesh3ng@gmail.com>
2022-05-23 22:54:34 +1000

    Add 05-scope

---
+++ 03-interJs/05-scope/scope.js
@@ -0,0 +1,17 @@
+// Scopes 
+
+// 1. Secruity 
+// ensures that variables can only be acessed to pre-defined 
+// parts of our program 
+
+// 2. Reduced variable name collisions 
+// reduces namespace collisions 
+// alows same variable name multiple times in different scopes 
+
+// global scope - the global space is JavaScript
+// local scope - created when a function is defined
+// block scope - created by entering a pair of curly braces
+
+// lexical scope -> lexical time 
+// js without dynamic scoping 
+// resolves varible names to their values when functions are nested 
