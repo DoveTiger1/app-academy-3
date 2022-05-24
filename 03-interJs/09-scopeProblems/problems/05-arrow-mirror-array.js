d102bfeeb37df78f8798550b14010e8c6faf4292
Ze <zesh3ng@gmail.com>
2022-05-24 19:53:37 +1000

    Passed 09-scopeProblems

---
+++ 03-interJs/09-scopeProblems/problems/05-arrow-mirror-array.js
@@ -0,0 +1,36 @@
+/***********************************************************************
+Write a function `arrowMirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples.
+
+Write this function using an arrow function!
+
+Examples:
+
+arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+// your code here
+
+const arrowMirrorArray = (arr) => { 
+  // iterate through each elenent in the array and push each element into the new array 
+  let newArr = arr.map((ele) => { 
+    return ele; 
+  }); 
+
+  // same thing however iterate backwards 
+  // to produce a mirrored array 
+  for (let i = arr.length - 1; i >= 0; i--) { 
+    newArr.push(arr[i]); 
+  }
+
+  return newArr; 
+}; 
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = arrowMirrorArray;
+} catch (e) {
+  return null;
+}
---
+++ 03-interJs/09-scopeProblems/problems/08-hidden-counter.js
@@ -0,0 +1,43 @@
+/***********************************************************************
+Write a function named `hiddenCounter()`. This function will do two things - first
+it will define a count variable, then it will return a function. 
+When invoked the function returned by hiddenCounter will increment the counter by 1.
+
+Look below to see how this function is invoked:
+
+let hidden1 = hiddenCounter(); //returns a function
+hidden1(); // returns 1
+hidden1(); // returns 2
+
+let hidden2 = hiddenCounter(); // returns a function
+hidden2(); // returns 1
+
+
+***********************************************************************/
+
+// your code here!
+
+const hiddenCounter = () => { 
+  let count = 0; 
+
+  // returns a function 
+  return () => { 
+    // increment count by one 
+    count++; 
+    // return count => reassigns the hidden 1 to 1
+    return count; 
+  }; 
+}; 
+
+let hidden1 = hiddenCounter(); //returns a function
+console.log(hidden1()); // returns 1
+console.log(hidden1()); // returns 2
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = hiddenCounter;
+} catch (e) {
+  // catch the ref err
+  module.exports = null;
+}
