f357649d6c16ec6193ad4eab00f6ede621a6839a
Ze <zesh3ng@gmail.com>
2022-05-27 22:56:31 +1000

    Update 15-subset to 11-recursionProblems

--- 03-interJs/11-recursionProblems/problems/15-subsets.js
+++ 03-interJs/11-recursionProblems/problems/15-subsets.js
@@ -15,14 +15,24 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 ***********************************************************************/
 
 // your code here
-function subsets(arr) { 
-  if (arr.length === 0) return []; 
 
-  return arr.reduce((subsets, value) => subsets.concat(
-     subsets.map(set => [value,...set])
-    ),
-    [[]]
-  );
+function subsets(nums) { 
+  // Recursively call function with nums[], less one element each time, until an empty set remains. At each step of the recursion stack:
+  // Take all the elements (subsets) returned from the previous step (called subsetsLessOne in this code) and add an element to it.
+  // This is done using the map() function here.
+  // Concatentenate the original elements returned from the previous step to the new elements created from the above step.
+
+  if (nums.length === 0) return [[]];
+  
+  // subsets([]) => [[]]
+  // subset([1]) => [[], [1]]
+  // subset([1, 2]) [[], [1], ]
+  let subsetsLessOne = subsets(nums.slice(0, nums.length - 1));
+
+  return subsetsLessOne.concat(subsetsLessOne.map(eachSubset => [...eachSubset, nums[nums.length - 1]]))
+
+  //     subset = [[]] my intial set    add subsets to [[]] => [[], [1]] => [[], [1]], [2]], [[1, 2]]
+  // return arr.reduce((subset, value) => subset.concat(subset.map(set => [...set, value])), [[]]);
 }
 
 console.log(subsets([1, 2, 3]));
