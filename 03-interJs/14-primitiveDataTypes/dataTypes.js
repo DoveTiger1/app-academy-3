d9ddc75af68d57e60c0576c52b0da719306fbb30
Ze <zesh3ng@gmail.com>
2022-05-29 21:50:39 +1000

    Add 14-primitiveDataTypes

---
+++ 03-interJs/14-primitiveDataTypes/dataTypes.js
@@ -0,0 +1,15 @@
+// Data Types 
+
+// Primitive Types 
+
+// Boolean - true and false
+// Null - represents the intentional absence of value.
+// Undefined - default return value for many things in JavaScript.
+// Number - like the numbers we usually use (15, 4, 42)
+// String - ordered collection of characters ('apple')
+// Symbol - new to ES5 a symbol is a unique primitive value
+// BigInt - a data type that can represent larger integers than the Number type can safely handle.
+
+// One Reference Types 
+
+// Object - a collection of properties and methods 
---
+++ 03-interJs/14-primitiveDataTypes/methods.js
@@ -0,0 +1,13 @@
+// Methods 
+
+// Function that belongs to an object
+// Primitive Types do not have methods  
+
+const corgi = { 
+    name: "Ein",
+    func: function() { 
+        console.log("This is a method"); 
+    }
+}; 
+
+corgi.func(); // This is a method 
