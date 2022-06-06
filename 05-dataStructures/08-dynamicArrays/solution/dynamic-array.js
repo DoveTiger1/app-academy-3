bcd2a83ce49e7f55756a5e401fbb234fa664710c
Ze <zesh3ng@gmail.com>
2022-06-07 00:17:17 +1000

    Pass `08-dynamicArray`

--- 05-dataStructures/08-dynamicArrays/solution/dynamic-array.js
+++ 05-dataStructures/08-dynamicArrays/solution/dynamic-array.js
@@ -3,17 +3,27 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
+    this.length = 0; 
+    this.capacity = defaultSize; 
+    this.data = []; 
+    this.data.length = defaultSize; 
 
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index]; 
   }
 
   unshift(val) {
 
-    // Your code here
+    // Your code here 
+    for (let i = this.length; i > 0; i--) { 
+      this.data[i] = this.data[i - 1]; 
+    }
+    this.data[0] = val;
+    this.length++; 
   }
 
 }
