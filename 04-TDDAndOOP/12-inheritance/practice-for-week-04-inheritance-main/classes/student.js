f85e05b52ef433a7479f40ed6f6f0d274359854c
Ze <zesh3ng@gmail.com>
2022-09-06 11:10:47 +1000

    Add `12-inheritance`

---
+++ 04-TDDAndOOP/12-inheritance/practice-for-week-04-inheritance-main/classes/student.js
@@ -0,0 +1,14 @@
+const Person = require('./person');
+
+// Your code here
+
+
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Student;
+} catch {
+  module.exports = null;
+}
