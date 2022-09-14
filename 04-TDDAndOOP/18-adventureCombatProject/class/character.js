41fcc37318ea21a7a1b064c4e2ec58cd04f39c38
Ze <zesh3ng@gmail.com>
2022-09-14 22:25:11 +1000

    add: pass all tests

---
+++ 04-TDDAndOOP/18-adventureCombatProject/class/character.js
@@ -0,0 +1,42 @@
+class Character {
+
+  constructor(name, description, currentRoom) {
+    // Fill this in
+    this.name = name; 
+    this.description = description; 
+    this.currentRoom = currentRoom; 
+
+    this.items = []; 
+    this.health = 100;
+    this.strength = 10; 
+  }
+
+  applyDamage(amount) {
+    // Fill this in
+    this.health -= amount;
+
+    if (this.health <= 0) {
+      this.health = 0; 
+    }
+
+    if (this.health === 0) { 
+      this.die(); 
+    } 
+  }
+
+  die() { 
+    // Fill this in
+    
+    this.items.forEach((item) => {
+      this.currentRoom.items.push(item); 
+      this.items.splice(this.items.indexOf(item), 1);
+    });
+
+    this.currentRoom = null; 
+  }
+
+}
+
+module.exports = {
+  Character,
+};
