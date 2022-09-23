14510a57a6eec5947f06ec92d9df0b62e1491731
Ze <zesh3ng@gmail.com>
2022-09-23 21:01:19 +1000

    refactor: clean up the logic statements

--- 04-TDDAndOOP/31-tttTDD/class/ttt.js
+++ 04-TDDAndOOP/31-tttTDD/class/ttt.js
@@ -20,25 +20,26 @@ class TTT {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand("c", "play against computer", TTT.comp.bind(this));
-    Screen.addCommand("r", "reset", TTT.reset.bind(this));
-    Screen.addCommand("return", "place move", TTT.place.bind(this));
-    Screen.addCommand("up", "move up", TTT.moveUp.bind(this));
-    Screen.addCommand("down", "move down", TTT.moveDown.bind(this));
-    Screen.addCommand("left", "move left", TTT.moveLeft.bind(this));
-    Screen.addCommand("right", "move right", TTT.moveRight.bind(this));
+    Screen.addCommand("c", "Play against computer", TTT.playComputer.bind(this));
+    Screen.addCommand("r", "Restart", TTT.restart.bind(this));
+    Screen.addCommand("space", "Place Move", TTT.place.bind(this));
+    Screen.addCommand("up", "Move Up", TTT.moveUp.bind(this));
+    Screen.addCommand("down", "Move Down", TTT.moveDown.bind(this));
+    Screen.addCommand("left", "Move Left", TTT.moveLeft.bind(this));
+    Screen.addCommand("right", "Move Right", TTT.moveRight.bind(this));
 
+    // Update messages 
     TTT.updateMsg.call(this);
     this.cursor.setBackgroundColor();
-
   }
 
   static updateMsg() {
     Screen.message = ` It is ${this.playerTurn}'s turn`;
   }
 
-  static reset() {
+  static restart() {
     this.playerTurn = "O";
+
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         Screen.grid[i][j] = " ";
@@ -55,13 +56,29 @@ class TTT {
     Screen.printCommands();
   }
 
-  static comp() {
-    
+  static playComputer() {
     this.computer = new ComputerPlayer();
-    TTT.reset();
+
+    TTT.restart();
+
     this.playerTurn = "O";
     Screen.message = "Playing against computer. It is your turn (O).";
 
+    Screen.render();
+    Screen.printCommands();
+  }
+
+  static computerMove() {
+    let move = ComputerPlayer.getSmartMove(Screen.grid, "X");
+
+    Screen.setGrid(move.row, move.col, "X");
+
+    if (TTT.checkWin(Screen.grid)) {
+      TTT.endGame(TTT.checkWin(Screen.grid));
+    }
+
+    this.playerTurn = "O";
+    Screen.message = "Playing against computer. It is your turn (O).";
     Screen.render();
     Screen.printCommands();
   }
@@ -69,141 +86,107 @@ class TTT {
   static moveUp()  {
     this.cursor.up();
   }
+
   static moveDown()  {
     this.cursor.down();
   }
+
   static moveLeft()  {
     this.cursor.left();
   }
+
   static moveRight()  {
     this.cursor.right();
   }
 
-  static computerMove() {
-    let move = ComputerPlayer.getSmartMove(Screen.grid, "X");
-    console.log(move);
-
-    Screen.setGrid(move.row, move.col, "X");
-
-    if(TTT.checkWin(Screen.grid)) {
-      TTT.endGame(TTT.checkWin(Screen.grid));
-    }
-    this.playerTurn = "O";
-    Screen.message = "Playing against computer. It is your turn (O).";
-    Screen.render();
-
-    Screen.printCommands();
-  }
-
   static place() {
+    Screen.render();
     if (Screen.grid[this.cursor.row][this.cursor.col] === " ") {
       Screen.setGrid(this.cursor.row, this.cursor.col, this.playerTurn);
 
       if(!this.computer) {
-        if (this.playerTurn === "O") {
-          this.playerTurn = "X";
-        } else {this.playerTurn = "O"}
+        this.playerTurn == "O" ? this.playerTurn = "X" : this.playerTurn = "O";
       }
-
 
       TTT.updateMsg.call(this);
       Screen.render();
       Screen.printCommands();
 
-      if(TTT.checkWin(Screen.grid)) {
-        TTT.endGame(TTT.checkWin(Screen.grid));
+      let winner = TTT.checkWin(Screen.grid); 
+      winner ? TTT.endGame(TTT.checkWin(Screen.grid)) : null;
+
+      if (this.computer) {
+        TTT.computerMove()
       }
-
-
-      if (this.computer) {TTT.computerMove()};
-
-
-
+      
     } else {
       TTT.updateMsg.call(this);
       Screen.message += "; invalid move";
+
       Screen.setBackgroundColor(this.cursor.row, this.cursor.col, "red");
       Screen.render();
       Screen.printCommands();
+    }
+  }
 
+  static flipGrid(grid) { 
+    let newGrid = []; 
+
+    for (const col in grid) { 
+      let flipped = grid.map(row => row[col]);
+      newGrid.push(flipped);
     }
 
+    return newGrid; 
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
 
-    //empty grid
-    let empty = true;
-    grid.forEach(
-      row => {
-        row.forEach(
-          cell => {
-            if (cell !== " ") {empty = false}
-          }
-        );
-      }
-    );
-    if (empty) {
-     return false;
-    }
+    const mirroredGrid = TTT.flipGrid(grid); 
 
-    //horizontal
-    let horizontalWinner;
-    grid.forEach(
-      row => {
-        if (row[0] !== " " && row[0] === row[1] && row[0] === row[2]) {
-          horizontalWinner = row[0];
+    let players = ["O", "X"]; 
+
+    for (const player in players) { 
+      // Check for horizontal wins
+      for (const row in grid) { 
+        if (grid[row].every(el => el == players[player])) { 
+          return players[player]; 
         }
       }
-    );
-    if (horizontalWinner) {
-      return horizontalWinner;
-    }
 
-    //vertical
-    let verticalWinner;
-    for (let i = 0; i < 3; i++) {
-      if (grid[0][i] !== " " && grid[0][i] === grid[1][i] && grid[0][i] === grid[2][i]) {
-        verticalWinner = grid[0][i];
+      // Check for vertical wins 
+      for (const col in mirroredGrid) { 
+        if (mirroredGrid[col]. every(el => el == players[player])) { 
+          return players[player]; 
+        }
+      }  
+
+      // Check for diagonal wins
+      if (
+        grid[0][0] == players[player] && 
+        grid[1][1] == players[player] &&
+        grid[2][2] == players[player]
+      ) { 
+        return players[player]; 
+      } else if (
+        grid[0][2] == players[player] && 
+        grid[1][1] == players[player] &&
+        grid[2][0] == players[player]
+      ) { 
+        return players[player]; 
       }
     }
-    if (verticalWinner) {
-      return verticalWinner;
+     // Check for tie game 
+    if (grid.every(row => row.every (v => v != ' '))) { 
+      return 'T'; 
+    } else { 
+      return false;
     }
-
-    //diagonal
-    let diagonalWinner;
-
-    if (grid[0][0] !== " " && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
-      diagonalWinner = grid[0][0];
-    }
-
-    if (grid[2][0] !== " " && grid[2][0] === grid[1][1] && grid[2][0] === grid[0][2]) {
-      diagonalWinner = grid[2][0];
-    }
-
-    if (diagonalWinner) {return diagonalWinner}
-
-    //tie
-    let tie = "T";
-    grid.forEach(
-      row => {
-        row.forEach(
-          cell => {
-            if (cell === " ") {tie = false}
-          }
-        );
-      }
-    );
-    return tie;
-
-    }
-
+  }
 
   static endGame(winner) {
     if (winner === 'O' || winner === 'X') {
